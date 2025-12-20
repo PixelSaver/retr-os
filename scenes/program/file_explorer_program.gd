15c2737a0968cacecfb879ebf0f493d447f4078c
PixelSaver <176425802+PixelSaver@users.noreply.github.com>
2025-12-20 11:11:56 -0800

    File Browser now works with OSFileBrowser component

--- scenes/program/file_explorer_program.gd
+++ scenes/program/file_explorer_program.gd
@@ -1,31 +1,56 @@
 extends Program
 class_name FileExplorerProgram
 
+var browser: OSFileBrowser
 var start_directory : String = ""
+var _prog_title: String = "Pixel Browser"
 
-func _program_start(args:Array=[]) -> void:
+func _program_start(args: Array = []) -> void:
+	print("%s started" % _prog_title)
+	
 	if args.size() > 0 and not (args[0] as String).is_empty():
 		start_directory = args[0]
-		
+		if DirAccess.dir_exists_absolute(start_directory):
+			browser.open_directory(start_directory)
 
 func _program_end() -> void:
-	pass
-	
+	print("%s ended" % _prog_title)
 func _program_ready() -> void:
-	hide()
-	var dialog = OSFileDialog.create(OSFileBrowser.FileMode.OPEN_FILE)
-	dialog.add_filter("Text Files", PackedStringArray(["txt"]))
-	dialog.add_filter("GDScript Files", PackedStringArray(["gd"]))
-	dialog.add_filter("All Files", PackedStringArray(["*"]))
+	title = _prog_title
 	
-	#add_child(dialog)
+	browser = OSFileBrowser.new()
+	browser.file_mode = OSFileBrowser.FileMode.OPEN_FILE
+	browser.allow_file_operations = true
+	browser.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
+	add_child(browser)
 	
-	Global.main_ui.window_container.add_child(dialog)
-	
-	dialog.file_selected.connect(_on_file_opened)
-	close_program_window()
+	browser.file_activated.connect(_on_file_activated)
+	browser.selection_changed.connect(_on_selection_changed)
 
-func _on_file_opened(path: String) -> void:
-	var file = FileAccess.open(path, FileAccess.READ)
-	if file:
-		Global.main_ui.run_program_by_id("text_editor", [path])
+## Attempt to open file with the correct program
+func _on_file_activated(path: String) -> void:
+	var ext = path.get_extension().to_lower()
+	
+	match ext:
+		"txt", "md", "gd", "json", "xml", "log":
+			Global.main_ui.run_program_by_id("text_editor", [path])
+		_:
+			# Ask what to open with
+			var confirm = OSConfirmationDialog.create(
+				"Open with Text Editor?",
+				"Open",
+				"Cancel"
+			)
+			get_window().get_parent().add_child(confirm)
+			confirm.confirmed.connect(func():
+				Global.main_ui.run_program_by_id("text_editor", [path])
+			)
+
+## Update window title
+func _on_selection_changed(paths: PackedStringArray) -> void:
+	if paths.size() == 1:
+		title = "%s - " % _prog_title + paths[0].get_file()
+	elif paths.size() > 1:
+		title = "%s - %d items selected" % [_prog_title, paths.size()]
+	else:
+		title = "%s - " % _prog_title + browser.get_current_dir().get_file()
